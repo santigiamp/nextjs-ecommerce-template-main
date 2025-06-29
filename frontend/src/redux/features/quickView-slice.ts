436b53853b66ddcaf76e2896de7a5e3877747a4b
Santino Giampá <santinogiampietro7@gmail.com>
2025-06-29 16:46:07 -0300

    Fix Product type initial state in quickView-slice.ts

--- frontend/src/redux/features/quickView-slice.ts
+++ frontend/src/redux/features/quickView-slice.ts
@@ -7,14 +7,16 @@ type InitialState = {
 
 const initialState = {
   value: {
-    title: "",
-    reviews: 0,
-    price: 0,
-    discountedPrice: 0,
-    img: "",
     id: 0,
-    images: [],
-    imgs: { thumbnails: [], previews: [] },
+    nombre: "",
+    precio: 0,
+    descripcion: "",
+    imagen_url: "",
+    categoria: "",
+    stock: 0,
+    precio_mayorista: 0,
+    minimo_mayorista: 0,
+    activo: false,
   } as Product,
 } as InitialState;
 
